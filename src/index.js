1143e39e6685a4de3e012d802517f0e8da81e198
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2021-09-25 00:28:24 +0300

    HW 10
    HW 10

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import menuItems from './menu.json';
 import menuCardTpl from './templates/menu-cards-markup.hbs';
 import refs from './js/refs';
+// Функция создания разметки
 function createMenuCardsMarkup (menuItems) {
-    return menuItems.map(menuCardTpl)
+        return menuItems.map(menuCardTpl)
         .join('');
 }
+
+
+const {menuList, themeCheckbox, body} = refs;
 
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
   };
-  refs.body.classList.add('light-theme');
-  refs.body.classList.add(localStorage.getItem('theme'));
+  
+// Устанавливаем тему по выбору и по умолнанию 
+function setTheme() {
+    let themePage = localStorage.getItem('theme'); 
+      if (themePage){
+    body.classList.add(localStorage.getItem('theme'));
+        if(localStorage.getItem('theme') === 'light-theme'){
+        themeCheckbox.checked = false;       
+        body.classList.add('light-theme');
+    }else {
+        themeCheckbox.checked = true;       
+    }
+    }else {
+        body.classList.add(Theme.LIGHT);
+        localStorage.setItem('theme', Theme.LIGHT)
+        console.log(Theme.LIGHT);
+    }
+}
 // Вешаем слушателя событий
-
-refs.themeCheckbox.addEventListener('change', (e) => {
+themeCheckbox.addEventListener('change', (e) => {
     if (e.target.checked) {
         localStorage.setItem('theme', Theme.DARK);
         document.querySelector('body').classList.replace('light-theme', 'dark-theme');
@@ -24,10 +43,7 @@ refs.themeCheckbox.addEventListener('change', (e) => {
     document.querySelector('body').classList.replace('dark-theme', 'light-theme')
     }
 });
-if(refs.body.classList.contains('dark-theme')){
-    refs.themeCheckbox.checked = true;
-}
 // Создаем динамически разметку   
 const menuMarkup = createMenuCardsMarkup(menuItems);
 refs.menuList.insertAdjacentHTML('beforeend', menuMarkup) ;
-
+setTheme();
